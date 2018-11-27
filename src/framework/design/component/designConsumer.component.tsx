23b5b1d054f65abedf10fce5ebfa6af52a955e38
Artur Yorsh <artyorsh.me@gmail.com>
2018-11-27 21:11:08 +0300

    feat(design): withDesign hoc

---
+++ src/framework/design/component/designConsumer.component.tsx
@@ -0,0 +1,52 @@
+import React, { ComponentType } from 'react';
+import {
+  DesignType,
+  MappingType,
+} from './type';
+import {
+  Consumer,
+  forwardProps,
+  getComponentMapping,
+} from '../service';
+
+export interface Props<P> extends React.ClassAttributes<P> {
+  mapping: MappingType;
+}
+
+export function withDesign<P extends Props<P>>(Component: ComponentType<P>) {
+  type TExcept = Exclude<keyof P, keyof Props<P>>;
+  type ForwardedProps = Pick<P, TExcept>;
+
+  class Shadow extends React.Component<ForwardedProps> {
+    wrappedComponentRef = undefined;
+    getWrappedInstance = undefined;
+
+    setWrappedComponentRef = (ref) => {
+      this.wrappedComponentRef = ref;
+    };
+
+    renderWrappedComponent = (design: [DesignType]) => (
+      <Component
+        ref={this.setWrappedComponentRef}
+        mapping={getComponentMapping(Component.displayName, design)}
+        {...this.props}
+      />
+    );
+
+    render() {
+      return (
+        <Consumer>
+          {this.renderWrappedComponent}
+        </Consumer>
+      );
+    }
+  }
+
+  const Result = Shadow;
+  Result.prototype.getWrappedInstance = function getWrappedInstance() {
+    const hasWrappedInstance = this.wrappedComponentRef && this.wrappedComponentRef.getWrappedInstance;
+    return hasWrappedInstance ? this.wrappedComponentRef.getWrappedInstance() : this.wrappedComponentRef;
+  };
+
+  return forwardProps(Component, Result);
+}
